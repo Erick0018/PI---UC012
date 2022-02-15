062f019eb12d05248f473065bfb8fed227618796
Erick0018 <erickontiveros017@gmail.com>
2022-02-15 14:13:24 -0300

    Projeto integrador

---
+++ AppRH/AppRH/src/main/java/com/AppRH/AppRH/controllers/BuscaController.java
@@ -0,0 +1,52 @@
+package com.AppRH.AppRH.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.AppRH.AppRH.repository.VagaRepository;
+import com.AppRH.AppRH.repository.CandidatoRepository;
+
+@Controller
+public class BuscaController {
+	
+	@Autowired
+	private VagaRepository vr;
+	
+	@Autowired
+	private CandidatoRepository cr;
+	
+	//GET
+	@RequestMapping("/")
+	public ModelAndView abrirIndex() {
+		ModelAndView mv = new ModelAndView("index");
+		return mv;
+	}
+	
+	//POST
+	@RequestMapping(value = "/", method = RequestMethod.POST)
+	public ModelAndView buscarIndex(@RequestParam("buscar") String buscar, @RequestParam("nome") String nome) {
+		
+		ModelAndView mv = new ModelAndView("index");
+		String mensagem = "Resultados da busca por " + buscar;
+			
+		if(nome.equals("nomecandidato")) {
+			mv.addObject("candidatos", cr.findByNomesCandidatos(buscar));
+			
+		}else if(nome.equals("titulovaga")) {
+			mv.addObject("vagas", vr.findByNomesVaga(buscar));
+			
+		}else {
+			mv.addObject("candidatos", cr.findByNomesCandidatos(buscar));
+			mv.addObject("vagas", vr.findByNomesVaga(buscar));
+		}
+		
+		mv.addObject("mensagem", mensagem);
+		
+		return mv;
+	}
+
+}
